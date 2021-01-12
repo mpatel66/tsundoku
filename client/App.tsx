a0f3bf2966dac1ba8bd93cc2055ec8950f8ca8d5
Meera <meera.patel2984@gmail.com>
2021-01-12 20:08:48 +0000

    Created skeleton react-native app

---
+++ client/App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+      <Button title="Click Me" onPress={() =>{console.log('Pressed')}}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
