a1f2d08d74e2cad0059e39035b8b522911e96077
Meera <meera.patel2984@gmail.com>
2021-01-14 08:58:54 +0000

    Reorganised TabNavigator component to a different file

--- client/App.tsx
+++ client/App.tsx
@@ -1,58 +1,26 @@
 import React, { useReducer } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { StyleSheet, View } from 'react-native';
-import Home from './screens/Home';
 import * as eva from '@eva-design/eva';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { NavigationContainer } from '@react-navigation/native';
-import { ApplicationProvider,  BottomNavigation,  BottomNavigationTab,  IconRegistry } from '@ui-kitten/components';
-import { BottomTabBarOptions, BottomTabBarProps, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MyBooks from './screens/MyBooks';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import Book from './Book';
 import BookModal from './screens/BookModal';
-import Search from './screens/Search';
-import UserSettings from './screens/UserSettings';
-import IconGenerator from './components/buttons/IconGenerator';
 import AppContext, { AppContextInterface } from './components/context/context'
+import TabNavigator from './components/navigation/TabNavigator';
 
 const queryClient = new QueryClient();
-const Tab = createBottomTabNavigator();
 
-const BottomTabBar = ({ navigation, state }: BottomTabBarProps<BottomTabBarOptions>) => {
-  return (
-    <BottomNavigation
-      selectedIndex={state.index}
-      onSelect={index => navigation.navigate(state.routeNames[index])}
-      style={styles.tab}
-      >
-      <BottomNavigationTab 
-        icon={(props) => 
-          <IconGenerator props={props} iconName={'home'}/>}
-        />
-      <BottomNavigationTab 
-        icon={(props) => 
-          <IconGenerator props={props} iconName={'book'}/>}/>
-      <BottomNavigationTab 
-        icon={(props) => 
-          <IconGenerator props={props} iconName={'search'}/>}/>
-      <BottomNavigationTab 
-        icon={(props) => 
-          <IconGenerator props={props} iconName={'settings-2'}/>}/>
-    </BottomNavigation>
-  );
+enum ActionType {
+  OPEN = 'OPEN_MODAL',
+  CLOSE = 'CLOSE_MODAL'
 }
 
+type Action =
+| { type: ActionType.OPEN; modalVisible: boolean; selectedBook: Book; }
+| { type: ActionType.CLOSE }
 
-const TabNavigator: React.FC = () => (
-  <Tab.Navigator tabBar={props => <BottomTabBar {...props} />}>
-    <Tab.Screen name='Home' component={Home}/>
-    <Tab.Screen name='MyBooks' component={MyBooks}/>
-    <Tab.Screen name='Search' component={Search}/>
-    <Tab.Screen name='Settings' component={UserSettings}/>
-  </Tab.Navigator>
-);
-
-function reducer(state:AppContextInterface, action:any) {
+function reducer(state:AppContextInterface, action:Action) {
   switch (action.type) {
     case 'OPEN_MODAL':
       return {
@@ -93,15 +61,3 @@ export default function App() {
     
   );
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  tab: {
-    height: '10%',
-  }
-});
---
+++ client/components/navigation/TabNavigator.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { BottomTabBarOptions, BottomTabBarProps, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {StyleSheet} from 'react-native';
+import { BottomNavigation,  BottomNavigationTab } from '@ui-kitten/components';
+import Home from '../../screens/Home';
+import MyBooks from '../../screens/MyBooks';
+import Search from '../../screens/Search';
+import UserSettings from '../../screens/UserSettings';
+import IconGenerator from '../buttons/IconGenerator';
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabBar = ({ navigation, state }: BottomTabBarProps<BottomTabBarOptions>) => {
+  return (
+    <BottomNavigation
+      selectedIndex={state.index}
+      onSelect={index => navigation.navigate(state.routeNames[index])}
+      style={styles.tab}
+      >
+      <BottomNavigationTab 
+        icon={(props) => 
+          <IconGenerator props={props} iconName={'home'}/>}
+        />
+      <BottomNavigationTab 
+        icon={(props) => 
+          <IconGenerator props={props} iconName={'book'}/>}/>
+      <BottomNavigationTab 
+        icon={(props) => 
+          <IconGenerator props={props} iconName={'search'}/>}/>
+      <BottomNavigationTab 
+        icon={(props) => 
+          <IconGenerator props={props} iconName={'settings-2'}/>}/>
+    </BottomNavigation>
+  );
+}
+
+
+const TabNavigator: React.FC = () => (
+  <Tab.Navigator tabBar={props => <BottomTabBar {...props} />}>
+    <Tab.Screen name='Home' component={Home}/>
+    <Tab.Screen name='MyBooks' component={MyBooks}/>
+    <Tab.Screen name='Search' component={Search}/>
+    <Tab.Screen name='Settings' component={UserSettings}/>
+  </Tab.Navigator>
+);
+
+
+const styles = StyleSheet.create({
+  tab: {
+    height: '10%',
+  }
+});
+
+export default TabNavigator;
